e2855ae6e89a88f8eae04e2b57fbc50107b0c61b
PurpleDeath <mcklink@bsugmail.net>
2015-10-23 21:39:40 -0600

    Base64 Encoder/Decoder written in PowerShell
    Script asks for encode or decode, then executes the selected action on
    given user input.

---
+++ Base64 encode and decode.ps1
@@ -0,0 +1,10 @@
+﻿Do{
+$var = Read-Host 'Would you like to encode or decode? Or type quit to exit.'
+    if ($var -eq 'encode')
+        {$text = Read-Host 'Enter the text that you would like to encode'
+        [System.Convert]::ToBase64String([System.Text.Encoding]::UNICODE.GetBytes($text))}
+    elseif($var -eq 'decode')
+        {$encoded_text = Read-Host 'Enter the encoded text that you would like to decode'
+        [System.Text.Encoding]::UNICODE.GetString([System.Convert]::FromBase64String($encoded_text))}
+    else{Write-host 'Please enter either encode or decode.'}
+}Until($var -eq 'quit')  
